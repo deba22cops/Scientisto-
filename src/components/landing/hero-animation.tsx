cfc1f96e3cd97ce5dfa1be883e3aeb849c632b30
DEBABRATA BEHERA <debabratadev6@gmail.com>
2025-09-26 17:16:14 +0000

    REMOVE THIS ANIMATION import React from 'react'; import { DotLottieReact

--- src/components/landing/hero-animation.tsx
+++ src/components/landing/hero-animation.tsx
@@ -5,13 +5,6 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 export function HeroAnimation() {
   return (
     <div className="absolute inset-0 z-0">
-        <div className="absolute inset-0 opacity-50">
-          <DotLottieReact
-            src="https://lottie.host/6b961c26-7004-4d33-8514-f27a6cb3bbc9/NutzC0pZjD.lottie"
-            loop
-            autoplay
-          />
-        </div>
         <div className="absolute inset-0">
          <DotLottieReact
           src="https://lottie.host/3226acff-d71c-4e70-82b3-701a9ae0bcf2/jdrnQ3CHNZ.lottie"

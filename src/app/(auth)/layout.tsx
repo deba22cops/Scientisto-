79089b2d4841cb9143ecd04d7a11dba05b69ea5b
DEBABRATA BEHERA <debabratadev6@gmail.com>
2025-09-26 18:05:34 +0000

    still now not implemented import React from 'react'; import { DotLottieR

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,8 +9,8 @@ export default function AuthLayout({
 }) {
   return (
     <div className="grid lg:grid-cols-2 min-h-screen bg-background">
-       <div className="flex flex-col items-center justify-center p-8 lg:order-last">
-        <div className="w-full max-w-sm">
+      <div className="hidden lg:flex flex-col items-center justify-center p-8 bg-muted/20">
+        <div className="w-full max-w-md">
           <DotLottieReact
             src="https://lottie.host/6b961c26-7004-4d33-8514-f27a6cb3bbc9/NutzC0pZjD.lottie"
             loop

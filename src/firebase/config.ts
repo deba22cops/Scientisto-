d835f651118a89937a367dac8b21865cd3eb21cc
DEBABRATA BEHERA <debabratadev6@gmail.com>
2025-09-26 17:57:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-430250886-afaff",
+  "appId": "1:182313377017:web:37c2cc9b4faf796bd29628",
+  "apiKey": "AIzaSyCiemo78XLrRVMXY7bXDu6Ok6IzxPOqFUA",
+  "authDomain": "studio-430250886-afaff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "182313377017"
+};
